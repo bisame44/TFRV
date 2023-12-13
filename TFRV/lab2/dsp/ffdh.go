a69a3bd1abb28e61d6fa1ca7de5eab7bff3a5b21
bisame44 <78916077+bisame44@users.noreply.github.com>
2023-12-13 22:00:53 +0700

    Update ffdh.go

--- TFRV/lab2/dsp/ffdh.go
+++ TFRV/lab2/dsp/ffdh.go
@@ -3,7 +3,7 @@ package dsp
 import (
 	"fmt"
 
-	"github.com/Belstowe/distrib-cs-1-autumn/lab2/tasks"
+	"https://github.com/bisame44/TFRV/tree/main/TFRV/lab2/tasks"
 )
 
 func FFDH(records []tasks.Task, n int) ([][]tasks.Task, error) {
